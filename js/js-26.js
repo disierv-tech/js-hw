18bc30d98bea48d3b5da46829d83781ee8843df4
disierv-tech <disierv@gmail.com>
2026-05-12 01:16:01 +0300

    homework for 26 chapter

---
+++ js/js-26.js
@@ -0,0 +1,40 @@
+// Створити телефонну книгу
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// створити set який прийматиме ім'я і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+
+// class Abonent {
+//   set contact(value) {
+//     this._name = value.name;
+//     this._phone = value.phone;
+//   }
+
+//   get contact() {
+//     return `Ім'я контакта: ${this._name}, номер телефону контакта: ${this._phone}`;
+//   }
+// }
+
+// const Ivan = new Abonent();
+// Ivan.contact = {
+//   name: "Ivan",
+//   phone: "+380680000001",
+// };
+
+// const Anna = new Abonent();
+
+// Anna.contact = {
+//   name: "Anna",
+//   phone: "+380680000002",
+// };
+
+// const Marta = new Abonent();
+// Marta.contact = {
+//   name: "Marta",
+//   phone: "+380680000003",
+// };
+
+// console.log(Ivan.contact);
+// console.log(Anna.contact);
+// console.log(Marta.contact);
